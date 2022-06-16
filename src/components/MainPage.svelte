4d497ee5db532c9131949bbd59be4b6ea0fa40f6
Rohit Karthik <rohit.karthik200@gmail.com>
2022-06-15 20:55:24 -0700

    finish

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,56 +1,113 @@
 <script>
     import MainItem from "./MainItem.svelte";
-    import { search } from "../logic/search";
+    import { hotelSearch, search } from "../logic/search";
+
+    let origin = "";
+    let destination = "";
+    let flightsList = [];
+    let hotelsList = [];
+    let date = "2022-06-16";
+    let outDate = "2022-06-24";
+    let loading = false;
+
+    function getSrcPic(airlineName) {
+        if (airlineName == "Delta Airlines") {
+            return "https://bit.ly/3Q02Jrp";
+        } else if (airlineName == "United Airlines") {
+            return "https://bit.ly/3MgHiiI";
+        } else {
+            return "https://bit.ly/3NhgLmF";
+        }
+    }
 </script>
 
 <header>
     <p class="heading">GoVenture</p>
     <div class="search">
-        <input type="date" class="padding" style="width: 150px;">
+        <label>
+            Arrival
+            <input
+                type="date"
+                class="padding"
+                style="width: 150px;"
+                bind:value={date}
+            />
+        </label>
+        <label>
+            Departure
+            <input
+                type="date"
+                class="padding"
+                style="width: 150px;"
+                bind:value={outDate}
+            />
+        </label>
         <input
+            bind:value={origin}
             class="padding"
-            style="width: 250px"
-            placeholder="Search for your destination:"
+            style="width: 175px"
+            placeholder="Where from?"
         />
-        <button class="padding btn btn-primary" on:click={search}>Search</button>
+        <input
+            bind:value={destination}
+            class="padding"
+            style="width: 175px"
+            placeholder="Where to?"
+        />
+        <button
+            class="padding btn btn-primary"
+            on:click={async () => {
+                loading = true;
+                const res = await search(origin, destination, date);
+                const res2 = await hotelSearch(destination, date, outDate)
+                flightsList = res["data"];
+                hotelsList = res2["data"]
+                loading = false;
+            }}>Search</button
+        >
     </div>
 </header>
 <div class="content">
-    <div class="items">
-        <p class="trifold">Flights</p>
-        <hr />
-        <MainItem
-        name="Delta Airlines"
-        price="500"
-        description="This is a desc"
-        src="https://bit.ly/3Q02Jrp"
-        alt="Delta Airlines"
-    />
-    <MainItem
-    name="American Airlines"
-    price="500"
-    description="This is a desc"
-    src="https://bit.ly/3NhgLmF"
-    alt="American Airlines"
-    />
-            <MainItem
-        name="United Airlines"
-        price="500"
-        description="This is a desc"
-        src="https://bit.ly/3MgHiiI"
-        alt="United Airlines"
-    />
-    </div>
-    <div class="vl" />
-    <div class="items">
-        <p class="trifold">Hotels</p>
-        <hr />
-    </div>
-    <div class="vl" />
-    <div class="items">
-        <p class="trifold">Rental Cars</p>
-        <hr />
-    </div>
+    {#if loading}
+        <div
+            style="display: flex; flex-direction: column; align-items: center; width: 100%;"
+        >
+            <h1>Searching for the best flights, hotels, and cars...</h1>
+            <div class="spinner-border text-primary" />
+        </div>
+    {:else}
+        <div class="items">
+            <p class="trifold">Flights</p>
+            <hr />
+            {#each flightsList as flight}
+                <MainItem
+                    name={flight["airline"]}
+                    price={flight["fare"]}
+                    description=""
+                    src={getSrcPic(flight["airline"])}
+                    alt={flight["airline"]}
+                />
+            {/each}
+        </div>
+        <div class="vl" />
+        <div class="items">
+            <p class="trifold">Hotels</p>
+            <hr />
+            {#each hotelsList as hotel}
+                <MainItem
+                    name={hotel["name"]}
+                    price={hotel["fare"]}
+                    description=""
+                    src="https://d2s2rtcxxwjegp.cloudfront.net/images/hotels/hotel_placeholder.png"
+                />
+            {/each}
+        </div>
+        <div class="vl" />
+        <div class="items">
+            <p class="trifold">Rental Cars</p>
+            <hr />
+        </div>
+    {/if}
 </div>
 
 <style>
--- src/logic/search.js
+++ src/logic/search.js
@@ -1,3 +1,10 @@
-export function search(destination) {
-    
+
+export async function search(origin, destination, date) {
+    const res = await fetch(`http://127.0.0.1:5000/flights/${origin}/${destination}/${date}`)
+    return await res.json()
+}
+
+export async function hotelSearch(city, inDate, outDate) {
+    const res = await fetch(`http://127.0.0.1:5000/hotels/${city}/${inDate}/${outDate}`)
+    return await res.json()
 }
